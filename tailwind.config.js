f03f747e3d77d6e297045253ba2ea5157a05eee0
IkBenJur <jurspammer@gmail.com>
2024-11-04 17:08:53 +0100

    Added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+
+    extend: {
+      colors: {
+        primary: "#141136",
+        secondary: "#2D2752",
+        accent: "#151136",
+      },
+    },
+  },
   plugins: [],
 };
