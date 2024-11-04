69d6cabfe77e266fa239465c05eb0a816aaf3165
IkBenJur <jurspammer@gmail.com>
2024-11-04 12:08:47 +0100

    Created home and hero-details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: "Home",
+    component: HomeComponent,
+  },
+  {
+    path: 'hero',
+    title: "Hero details",
+    component: HeroDetailsComponent,
+  },
+];
