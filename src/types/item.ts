81468e763482d8a39ca6849cf4b23081a484c7a5
IkBenJur <jurspammer@gmail.com>
2024-11-07 11:17:26 +0100

    Created new type

---
+++ src/types/item.ts
@@ -0,0 +1,13 @@
+export type Item = {
+    itemId: number;
+    name: string;
+    cost: number;
+    type: string;
+    active: boolean;
+    effects: [
+        {
+            itemEffectId: number;
+            effect: string;
+        }
+    ]
+}
