6acaaf89c0018a8873a04db8375683acc1f33db5
IkBenJur <jurspammer@gmail.com>
2024-11-07 11:17:54 +0100

    Created new service

---
+++ src/app/item.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Item } from '../types/item';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  private url = "http://localhost:8082/item"
+
+  constructor(private http: HttpClient) { }
+
+  getByType(type: string): Observable<Item[]> {
+    return this.http.get<Item[]>(`${this.url}/${type}`)
+  }
+}
