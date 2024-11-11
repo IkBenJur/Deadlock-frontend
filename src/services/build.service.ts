081b1f0ceccdcc37a14ff4f29b790232b675e0ba
IkBenJur <jurspammer@gmail.com>
2024-11-11 13:50:50 +0100

    Created new build service

--- src/services/build.service.ts
+++ src/services/build.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Build } from '../types/build';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BuildService {
+  private url = "http://localhost:8082/build"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createNew(build: Build): Observable<any> {
+    build.createdAt = new Date();
+    return this.http.post(`${this.url}/`, build)
+  }
+
+  getAll(): Observable<Build[]> {
+    return this.http.get<Build[]>(`${this.url}`)
+  }
 }
