c8c984f417cac94e0556717e728a3ba985fc7e3a
IkBenJur <jurspammer@gmail.com>
2024-11-11 16:36:06 +0100

    Now uses item-card component

--- src/app/item-tab-item/item-tab-item.component.ts
+++ src/app/item-tab-item/item-tab-item.component.ts
@@ -3,11 +3,12 @@ import { Item } from '../../types/item';
 import { ItemService } from '../../services/item.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { ItemCardComponent } from "../item-builds/item-card/item-card.component";
 
 @Component({
   selector: 'app-item-tab-item',
   standalone: true,
-  imports: [AsyncPipe, CommonModule],
+  imports: [AsyncPipe, CommonModule, ItemCardComponent],
   templateUrl: './item-tab-item.component.html',
 })
 export class ItemTabItemComponent {
